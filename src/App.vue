a74163354fdc90ade5413f004316a030797e93ed
andikashln <andikashalihin01@gmail.com>
2024-05-04 10:46:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 1. Beberapa elemen HTML yang berbeda -->
+    <h1 style="font-family: Georgia, 'Times New Roman', Times, serif;">Welcome!!</h1>
+
+    <!-- 2. Elemen input teks -->
+    <input v-model="inputText" placeholder="Masukkan teks di sini" />
+
+    <!-- 3. Penggunaan class bindings dan style bindings -->
+    <div :class="{ 'highlighted': isHighlighted }" :style="{ color: textColor }">
+      <h2><ul>coba masukkan teks apapun </ul></h2>
+    </div>
+
+    <!-- 4. Interaksi dinamis dengan event listener -->
+    <button @click="changeText">Ubah Teks</button>
+    <p>{{ dynamicText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      isHighlighted: false,
+      textColor: 'blue',
+      dynamicText: 'Teks ini akan berubah!',
+    };
+  },
+  methods: {
+    changeText() {
+      this.dynamicText = 'Teks telah diubah!';
+      this.isHighlighted = !this.isHighlighted;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.highlighted {
+  background-color: plum;
+}
+h1 {
+  margin-bottom: 10%;
+}
+
+</style>
